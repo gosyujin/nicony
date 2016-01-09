4825a727cce16fcaa0af521a2b379f354885ee2c
kk_Ataka <kk_ataka@ring.skr.jp>
2016-01-09 09:14:09 +0900

    logger分割

--- nicony.go
+++ nicony.go
@@ -108,46 +108,6 @@ type ThreadKeyInfo struct {
 // cookie
 var jar, _ = cookiejar.New(nil)
 
-func initLogger(o Option) {
-	// seelog設定
-	var logConfig string
-	if *o.IsAnsi {
-		logConfig = `
-		  <seelog type="adaptive" mininterval="200000000" maxinterval="1000000000" critmsgcount="5">
-		    <formats>
-		      <format id="console" format="%EscM(36)[nicony]%EscM(39) %EscM(32)%Date(2006-01-02T15:04:05Z07:00)%EscM(39) %EscM(33)[%File:%FuncShort:%Line]%EscM(39) %EscM(46)[%LEVEL]%EscM(49) %Msg%n" />
-		      <format id="plane" format="[nicony] %Date(2006-01-02T15:04:05Z07:00) [%File:%FuncShort:%Line] [%LEVEL] %Msg%n" />
-		    </formats>
-		    <outputs>
-		      <filter formatid="console" levels="debug,info,warn,error,critical">
-		        <console />
-		      </filter>
-		      <filter formatid="plane" levels="trace,debug,info,warn,error,critical">
-		        <rollingfile filename="./log/log.txt" type="size" maxsize="1024000" maxrolls="500" />
-		      </filter>
-		    </outputs>
-		  </seelog>`
-	} else {
-		logConfig = `
-		  <seelog type="adaptive" mininterval="200000000" maxinterval="1000000000" critmsgcount="5">
-		    <formats>
-		      <format id="plane" format="[nicony] %Date(2006-01-02T15:04:05Z07:00) [%File:%FuncShort:%Line] [%LEVEL] %Msg%n" />
-		    </formats>
-		    <outputs>
-		      <filter formatid="plane" levels="debug,info,warn,error,critical">
-		        <console />
-		      </filter>
-		      <filter formatid="plane" levels="trace,debug,info,warn,error,critical">
-		        <rollingfile filename="./log/log.txt" type="size" maxsize="1024000" maxrolls="500" />
-		      </filter>
-		    </outputs>
-		  </seelog>`
-	}
-
-	logger, _ := log.LoggerFromConfigAsBytes([]byte(logConfig))
-	log.ReplaceLogger(logger)
-}
-
 func main() {
 	o := Option{}
 	o.IsAnsi = flag.Bool("ansi", true, "Output Ansi color")
@@ -156,6 +116,7 @@ func main() {
 
 	initLogger(o)
 	defer log.Flush()
+
 	log.Info("nicony ver.0.3")
 
 	login()
