4d273e46dbecf2c9fe1feac7662e4c68bf75f11e
kk_Ataka <kk_ataka@ring.skr.jp>
2016-01-13 19:08:14 +0900

    aaa=bbb&ccc=dddのクエリを構造体に入れたいんだけど、できない
    クエリ->map->json->structで行けるかと思ったけど、クエリ->mapの時点(url.ParseQuery)で
    aaa:["bbb"]のように配列になっちゃうので非常に使いづらい
    
    http://kwmt27.net/index.php/2013/10/13/how-to-convert-from-map-to-struct/
    http://blog.restartr.com/2014/08/13/golang-json-marshal-unmarshal/

--- flvinfo.go
+++ flvinfo.go
@@ -1,36 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	log "github.com/cihub/seelog"
 	"io/ioutil"
 	"net/http"
-	"strings"
+	"net/url"
 )
 
 // 指定された動画のFLV保管URLの情報 http://dic.nicovideo.jp/a/ニコニコ動画api
 type FlvInfo struct {
-	ThreadId         string //1 コメントDLで使う
-	L                string //2 コメントDLで使う、60で割って+1して使う
-	Url              string //3 動画DLで使う
-	Ms               string //4 コメントDLで使う
-	MsSub            string //5
-	UserId           string //6 コメントDLで使う
-	IsPremium        string //7 (プレミアムなら1)
-	Nickname         string //8
-	Time             string //9
-	Done             string //10
-	NgRv             string //11
-	Hms              string //12
-	Hmsp             string //13
-	Hmst             string //14
-	Hmstk            string //15
-	UserKey          string //16
-	NeedsKey         string //17 公式放送のみ存在？
-	OptionalThreadId string //18 公式放送のみ存在？
-	NgCh             string //19 公式放送のみ存在？
-	Closed           string //20
-	Deleted          string //21
-	Error            string //22
+	ThreadId         string   `json:"thread_id"`          //1 コメントDLで使う
+	L                string   `json:"l"`                  //2 コメントDLで使う、60で割って+1して使う
+	Url              string   `json:"url"`                //3 動画DLで使う
+	Ms               string   `json:"ms"`                 //4 コメントDLで使う
+	MsSub            string   `json:"ms_sub"`             //5
+	UserId           string   `json:"user_id"`            //6 コメントDLで使う
+	IsPremium        string   `json:"is_premium"`         //7 (プレミアムなら1)
+	Nickname         string   `json:"nickname"`           //8
+	Time             []string `json:"time"`               //9
+	Done             string   `json:"done"`               //10
+	NgRv             string   `json:"ng_rv"`              //11
+	Hms              string   `json:"hms"`                //12
+	Hmsp             string   `json:"hmsp"`               //13
+	Hmst             string   `json:"hmst"`               //14
+	Hmstk            string   `json:"hmstk"`              //15
+	UserKey          string   `json:"user_key"`           //16
+	NeedsKey         string   `json:"needs_key"`          //17 公式放送のみ存在？
+	OptionalThreadId string   `json:"optional_thread_id"` //18 公式放送のみ存在？
+	NgCh             string   `json:"ng_ch"`              //19 公式放送のみ存在？
+	Closed           string   `json:"closed"`             //20
+	Deleted          string   `json:"deleted"`            //21
+	Error            string   `json:"error"`              //22
 }
 
 func getFlvInfo(getFlvUrl string) FlvInfo {
@@ -39,68 +40,23 @@ func getFlvInfo(getFlvUrl string) FlvInfo {
 	log.Debug("get getFlvUrl " + getFlvUrl)
 	res, _ := client.Get(getFlvUrl)
 	body, _ := ioutil.ReadAll(res.Body)
+	log.Trace(string(body))
 	log.Debug(res.Status)
 	defer res.Body.Close()
 
-	//レスポンスをクエリパラメータ毎に分割
-	f := FlvInfo{}
-	for _, param := range strings.Split(string(body), "&") {
-		temp := strings.Split(param, "=")
-		key := temp[0]
-		value := temp[1]
+	// クエリパラメータをいったんmap -> json -> 構造体フィールドタグに落としこむ
+	queryMap, _ := url.ParseQuery(string(body))
+	log.Tracef("%#v", queryMap)
 
-		switch key {
-		case "thread_id":
-			f.ThreadId = value
-		case "l":
-			f.L = value
-		case "url":
-			f.Url = value
-		case "ms":
-			f.Ms = value
-		case "ms_sub":
-			f.MsSub = value
-		case "user_id":
-			f.UserId = value
-		case "is_premium":
-			f.IsPremium = value
-		case "nickname":
-			f.Nickname = value
-		case "time":
-			f.Time = value
-		case "done":
-			f.Done = value
-		case "ng_rv":
-			f.NgRv = value
-		case "hms":
-			f.Hms = value
-		case "hmsp":
-			f.Hmsp = value
-		case "hmst":
-			f.Hmst = value
-		case "hmstk":
-			f.Hmstk = value
-		case "userkey":
-			f.UserKey = value
-		case "needs_key":
-			f.NeedsKey = value
-		case "optional_thread_id":
-			f.OptionalThreadId = value
-		case "ng_ch":
-			f.NgCh = value
-		case "closed":
-			// 1だと不正終了っぽい
-			f.Closed = value
-		case "deleted":
-			// 2だと削除された動画っぽい
-			f.Deleted = value
-		case "error":
-			// invalid_v1だと有料放送っぽい？
-			f.Error = value
-		default:
-			log.Warn("unknown parameter: " + key + " value is " + value)
-		}
+	j, _ := json.Marshal(queryMap)
+	log.Tracef("%#v", string(j))
+
+	flvInfo := FlvInfo{}
+	err := json.Unmarshal(j, &flvInfo)
+	if err != nil {
+		log.Error(err)
 	}
+	log.Tracef("%#v", flvInfo)
 
 	//TODO 必須パラメータ存在チェック
 	//log.Debug(f.ThreadId)
@@ -108,5 +64,5 @@ func getFlvInfo(getFlvUrl string) FlvInfo {
 	//log.Debug(f.Ms)
 	//log.Debug(f.UserId)
 
-	return f
+	return flvInfo
 }
